67c8f3797882dedb6d72df9df647f6a0b44f0ee6
AnshulaSharma <anshula.17@gmail.com>
2020-04-06 14:30:09 +0200

    Add project files.

---
+++ SudokuApp/Controllers/SudokuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SudokuApp.Model;
+
+namespace SudokuApp.Controllers
+{
+    [Route("[controller]")]
+    public class SudokuController : Controller
+    {
+      
+        [Route("solution")]
+        [HttpPost]
+        public void Solution([FromBody] Puzzle sudokuPuzzle)
+        {
+            try
+            {
+              
+            }
+            catch
+            {
+        
+            }
+        }
+
+    }
+}
